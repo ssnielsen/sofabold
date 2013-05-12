d22aa4d35d1b86922fc9b66d16b867d285c54e6d
ssnielsen <ssni@itu.dk>
2013-05-13 00:56:34 +0200

    Minor changes

--- application/views/matches_view.php
+++ application/views/matches_view.php
@@ -1,8 +1,8 @@
-<div class="clusters">
+<div class="matches">
 	<div class="inline"><h1>Fodbold</h1></div>
 	<br class="clearBoth" />
 
-	<div class="matches">
+	<div class="matches_table">
 		<table class="table table-striped table-bordered standard">
 			<tbody>
 				<?php 
@@ -10,8 +10,8 @@
 						<tr>
 							<td>
 								<div class="time-style">
-									<div class="prematch-time">Optakt starter kl. 14:00</div> 
-									<div class="match-time">Kampen starter kl. 15:00</div> 
+									<div class="prematch-time">Optakt starter: 14:00</div> 
+									<div class="match-time">Kampen starter: 15:00</div> 
 								</div>
 								<br>
 								<?php print($match['home'] . ' - ' . $match['away']); ?>
