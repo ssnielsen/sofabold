1d96b95c0a80661d6bac7c0bf893523af4b4876a
Søren Sønderby Nielsen <ssn231090@gmail.com>
2013-08-26 14:34:05 +0200

    for now...

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -16,19 +16,19 @@
 	<script type="text/javascript" src="<?= base_url() ?>static/js/tipsy.js"></script>
 	<script type="text/javascript" src="<?= base_url() ?>static/js/bootstrap-alert.js"></script>
 	<link rel="stylesheet" type="text/css" href="<?= base_url() ?>static/css/960.css">
-	<link rel="stylesheet" type="text/css" href="<?= base_url() ?>static/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="<?= base_url() ?>static/css/bootstrap.css">
 	<link rel="stylesheet" type="text/css" href="<?= base_url() ?>static/css/tipsy.css">
 	<link rel="stylesheet" type="text/css" href="<?= base_url() ?>static/css/main.css">
 </head>
 <body>
 	<header id="main">
 		<nav class="navbar navbar-inverse">
-			<div class="navbar-inner">
+			<div class="navbar-inner" style="-webkit-border-radius: 0; -moz-border-radius: 0; border-radius: 0;">
 				<div class="container_24">
 					<a class="brand" href="<?= base_url() ?>"><img src="<?= base_url() ?>static/img/header_logo.png">  Sofabold</a>
 					<ul class="nav">
 						<li><a href="<?= base_url() ?>index.php/matches">Kampe</a></li>
-						<li><a href="<?= base_url() ?>index.php/admin">Admin</a></li>
+						<!-- <li><a href="<?= base_url() ?>index.php/admin">Admin</a></li> -->
 					</ul>
 				</div>
 			</div>
